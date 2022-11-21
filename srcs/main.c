3f5c9e67f7c59db89491e4dad7260973fb0513bf
Loic Dubouloz <lulutalu@gmail.com>
2022-11-21 13:55:16 +0100

    Resolve issue with direction of right vector of camera and avoid mlx window launching when parsing return an error

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ngda-sil <ngda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 14:54:21 by lduboulo          #+#    #+#             */
-/*   Updated: 2022/11/08 22:47:25 by ngda-sil         ###   ########.fr       */
+/*   Updated: 2022/11/18 16:59:02 by lduboulo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	main(int ac, char **av)
 {
 	t_main	m;
 
-	rt_init(&m.mlx);
 	parse(ac, av, &m);
+	rt_init(&m.mlx);
 	camera_init(&m.mlx, &m.cam, &m.scn);
 	mlx_hook(m.mlx.window, 2, (1L << 13), keyhook, &m);
 	frame_loop(&m);
